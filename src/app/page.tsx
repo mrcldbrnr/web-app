d1237e878fb78e0e486d7eaf5b9e487b37c0b097
mrcldbrnr <53258821+mrcldbrnr@users.noreply.github.com>
2026-08-16 12:42:31 +0200

    startseite: verlauf korrigieren

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function DashboardPage() {
     <div className="relative overflow-x-hidden">
       <div
         aria-hidden="true"
-        className="pointer-events-none absolute top-0 left-1/2 -z-10 h-[32rem] w-screen -translate-x-1/2 bg-[radial-gradient(60%_60%_at_50%_0%,#e0e0ff,transparent)] opacity-70"
+        className="pointer-events-none absolute -top-6 left-1/2 -z-10 h-[32rem] w-screen -translate-x-1/2 bg-[radial-gradient(ellipse_farthest-side_at_top,#e0e0ff,transparent)] opacity-70 sm:-top-8"
       />
 
       <div className="space-y-10 sm:space-y-12">
